8787e73f1aeec0799f9c642c600cef45437b6bcd
kahkyy <124581885+kahkyy@users.noreply.github.com>
2024-03-06 23:24:51 +0200

    Users.cpp

--- Users.cpp
+++ Users.cpp
@@ -4,3 +4,19 @@ QString usernames[100] = {"admin", "test", "user1", "user2"};
 QString passwords[100] = {"admin", "1234", "1101", "1102"};
 int ages[100] = {20, 21, 22, 23};
 int usersCount = 4;
+
+bool isUsernameExists(const QString& username) {
+    for (int i = 0; i < usersCount; ++i) {
+        if (usernames[i] == username) {
+            return true;
+        }
+    }
+    return false;
+}
+void addUser(const QString& username, const QString& password, int age) {
+    usernames[usersCount] = username;
+    passwords[usersCount] = password;
+    ages[usersCount] = age;
+
+    ++usersCount;
+}
