6e8488d273a15c4d6d9e4823cbd1f5e580f72854
kahkyy <124581885+kahkyy@users.noreply.github.com>
2024-03-06 23:26:07 +0200

    Users.h

--- Users.h
+++ Users.h
@@ -7,4 +7,7 @@ extern QString passwords[100];
 extern int ages[100];
 extern int usersCount;
 
+bool isUsernameExists(const QString& username);
+void addUser(const QString& username, const QString& password, int age);
+
 #endif // USERS_H
